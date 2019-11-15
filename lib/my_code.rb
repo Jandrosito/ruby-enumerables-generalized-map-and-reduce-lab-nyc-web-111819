9c61237fa7880b5929b5ad441d38f2b7b1b8ab3c
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 05:15:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array.push(yield(array[i]))
+    i += 1
+  end
+  new_array
+end
+
+def reduce(array, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = array[0]
+    i = 1
+  end
+  while i < array.length
+    accum = yield(accum, array[i])
+    i += 1
+  end
+  accum
+end
